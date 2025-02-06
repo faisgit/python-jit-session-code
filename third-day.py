84b0fa3483b3d75d4f6946cf82effdfad1a53a51
faisgit <fa830163@gmail.com>
2025-02-06 11:39:12 +0530

    Inheritence

--- third-day.py
+++ third-day.py
@@ -16,6 +16,21 @@ class Account:
         else:
             print("Insufficient Balance")
 
+class SavingsAccount(Account):
+    def __init__(self, account_number,balance, interest_rate):
+        super().__init__(account_number,balance)
+        self.interest_rate = interest_rate
+    
+    def calculate_interest(self):
+        interest = self.balance  * self.interest_rate
+        self.balance += interest
+        print(f"Interest of {interest} calculated and added. New Balance: {self.balance}")
+
+Sujal  = SavingsAccount('12300190',5000, 10)
+Sujal.deposit(3000)
+
+        
+
 # account = Account('12345678', 2000)
 # print(account.get_balance())
 piyush = Account('200000000000000000000',500000)
